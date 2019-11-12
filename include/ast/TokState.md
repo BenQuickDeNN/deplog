4e646521f789af77a8ff99eac03fac7100a76059
BenQuickDeNN <benquickdenn@foxmail.com>
2019-11-12 18:34:40 +0800

    add TokState.md

--- include/ast/TokState.md
+++ include/ast/TokState.md
@@ -1,5 +1,30 @@
+# FSM of lexer
 ```mermaid
 graph TD
-    START --space--> S1
-    S1 --space--> S1
+    START-->S1
+
+    S1--blank-->S1
+    S1--alpha-->S2
+    S1--digit-->S3
+    S1--symbol-->S5
+
+    S2--alpha/digit-->S2
+    S2--blank/symbol-->IDENTITY
+    IDENTITY==keyword==>KEYWORD
+
+    S3--digit-->S3
+    S3--blank/symbol-->INT_NUM
+    S3--dot-->S4
+    S4--digit-->S4
+    S4--blank/symbol-->FLOAT_NUM
+
+    S5--symbol-->S5
+    S5--blank/digit/alpha-->SYMBOL
+    SYMBOL==comment_l==>COMMENT_L
+    COMMENT_L--else-->COMMENT_L
+    COMMENT_L--COMMENT_R-->COMMENT
+    SYMBOL==comment_r==>COMMENT_R
+    SYMBOL==comment_line==>COMMENT_LINE
+    COMMENT_LINE--else-->COMMENT_LINE
+    COMMENT_LINE--next_line-->COMMENT
 ```
--- include/ast/lexer.cxx
+++ include/ast/lexer.cxx
@@ -1,15 +1,67 @@
+#include <cctype>
 #include <string>
+#include <vector>
 
 namespace deplog
 {
+    static const std::vector<std::string> DictKeyWord;
+    static const std::vector<std::string> DictOperator;
+    static const std::vector<std::string> DictUnaryOperator;
     enum TokType;
     enum TokState;
     class Token;
+    std::vector<Token> getTokens(const std::string& code);
+    const bool& issymbol(const char& c);
+    const bool& isdot(const char& c);
+    const bool& iscomment_l(const std::string& str);
+    const bool& iscomment_r(const std::string& str);
+    const bool& iscomment_line(const std::string& str);
+    const bool& isnext_line(const char& c);
 }
+const bool& deplog::isnext_line(const char& c)
+{
+    return c == '\n';
+}
+const bool& deplog::iscomment_line(const std::string& str)
+{
+    return str.c_str == "//";
+}
+const bool& deplog::iscomment_r(const std::string& str)
+{
+    return str.c_str() == "*/";
+}
+const bool& deplog::iscomment_l(const std::string& str)
+{
+    return str.c_str() == "/*";
+}
+const bool& deplog::issymbol(const char& c)
+{
+    /* check ASCII range */
+    return ((0x21 <= c && c <= 0x2F) ||
+        (0x3A <= c && c <= 0x40) || (0x5B <= c && c <= 0x60) ||
+        (0x7B <= c && c <= 0x7E)) && !(isdot(c) || isnext_line(c));
+}
+const bool& deplog::isdot(const char& c)
+{
+    return c == '.';
+}
+static const std::vector<std::string> deplog::DictKeyWord = {
+    "for"
+};
+static const std::vector<std::string> deplog::DictOperator = {
+    "<",
+    ">",
+    "+",
+    "-"
+};
+static const std::vector<std::string> deplog::DictUnaryOperator = {
+    "++",
+    "--"
+};
 enum deplog::TokType
 {
     /* keyword */
-    tok_for             ,   // for
+    tok_keyword         ,   // when scanning an identity, check if it is a keyword
     /* operator */
     tok_less_than       ,   // <
     tok_greater_than    ,   // >
@@ -18,16 +70,10 @@ enum deplog::TokType
     tok_postfix_sub     ,   // --
     /* assignment */
     tok_assign          ,   // =
-    /* symbol */
-    tok_parenthese_l    ,   // (
-    tok_parenthese_r    ,   // )
-    tok_bracket_l       ,   // [
-    tok_bracket_r       ,   // ]
-    tok_brace_l         ,   // {
-    tok_brace_r         ,   // }
-    tok_semi            ,   // ;
+    /* single symbol */
+    tok_single_symbol   ,
     /* dynamic token */
-    tok_identify        ,   // [a-zA-Z][a-zA-Z0-9_]*
+    tok_identity        ,   // [a-zA-Z][a-zA-Z0-9_]*
     tok_int_num         ,   // -+[0-9]+
     tok_float_num       ,   // -+[0-9]+\.[0-9]*
     tok_bytes               // 0x[0-9a-fA-F]+
@@ -35,7 +81,21 @@ enum deplog::TokType
 enum deplog::TokState
 {
     START,
-    S1
+    LESS_THAN_1,    // <
+    GREATER_THAN_1, // >
+    ADD_1,          // +
+    ADD_2,          // ++
+    SUB_1,          // -
+    SUB_2,          // --
+    ASSIGN_1,       // =
+    ASSIGN_2,       // ==
+    PARENTHESE_L_1, // (
+    PARENTHESE_R_1, // )
+    BRACKET_L_1,    // [
+    BRACKET_R_2,    // ]
+    BRACE_L_1,      // {
+    BRACE_R_1,      // }
+    SEMI_1,         // ;
 };
 class deplog::Token
 {
