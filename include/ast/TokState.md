59d519c2b9eb201f038f43d9622773447c9594b5
BenQuickDeNN <benquickdenn@foxmail.com>
2019-11-13 20:34:07 +0800

    need to add comment token

--- include/ast/TokState.md
+++ include/ast/TokState.md
@@ -7,24 +7,31 @@ graph TD
     S1--alpha-->S2
     S1--digit-->S3
     S1--symbol-->S5
+    S1--0-->S6
 
     S2--alpha/digit-->S2
-    S2--blank/symbol-->IDENTITY
+    S2--blank/symbol/ctrl-->IDENTITY
     IDENTITY==keyword==>KEYWORD
 
     S3--digit-->S3
-    S3--blank/symbol-->INT_NUM
+    S3--blank/symbol/ctrl-->INT_NUM
     S3--dot-->S4
     S4--digit-->S4
-    S4--blank/symbol-->FLOAT_NUM
+    S4--blank/symbol/ctrl-->FLOAT_NUM
 
     S5--symbol-->S5
-    S5--blank/digit/alpha-->SYMBOL
+    S5--blank/digit/alpha/ctrl-->SYMBOL
     SYMBOL==comment_l==>COMMENT_L
     COMMENT_L--else-->COMMENT_L
     COMMENT_L--COMMENT_R-->COMMENT
     SYMBOL==comment_r==>COMMENT_R
     SYMBOL==comment_line==>COMMENT_LINE
     COMMENT_LINE--else-->COMMENT_LINE
-    COMMENT_LINE--next_line-->COMMENT
+    COMMENT_LINE--ctrl-->COMMENT
+
+    S6--"x"-->S7
+    S6--digit-->S3
+    S7--"digit/[a-fA-F]"-->S8
+    S8--"digit/[a-fA-F]"-->S8
+    S8--blank/symbol/ctrl-->HEX_NUM
 ```
--- include/ast/lexer.cxx
+++ include/ast/lexer.cxx
@@ -10,17 +10,146 @@ namespace deplog
     enum TokType;
     enum TokState;
     class Token;
-    std::vector<Token> getTokens(const std::string& code);
+    /**
+     * @brief get token serial
+     * @param code content
+     * @return token vector
+    */
+    const std::vector<Token>& getTok(const std::string& code);
+    void lexerror(const int& idx, const char& c, TokState& tokstate);
     const bool& issymbol(const char& c);
     const bool& isdot(const char& c);
     const bool& iscomment_l(const std::string& str);
     const bool& iscomment_r(const std::string& str);
     const bool& iscomment_line(const std::string& str);
-    const bool& isnext_line(const char& c);
+    const bool& iskeyword(const std::string& str);
 }
-const bool& deplog::isnext_line(const char& c)
+const std::vector<deplog::Token>& deplog::getTok(const std::string& code)
 {
-    return c == '\n';
+    std::vector<deplog::Token> res;
+    TokState tokstate = TokState::START;
+    tokstate =  TokState::S1;
+    std::string value = "";
+    for (unsigned int idx = 0; idx < code.size(); idx++)
+    {
+        if (tokstate == TokState::S1)
+        {
+            if (std::isblank(code[idx]))
+                tokstate = TokState::S1;
+            else if (std::isalpha(code[idx]))
+                tokstate = TokState::S2;
+            else if (std::isdigit(code[idx]))
+                tokstate = TokState::S3;
+            else if (issymbol(code[idx]))
+                tokstate = TokState::S5;
+            else
+            {
+                lexerror(idx, code[idx], tokstate);
+                continue;
+            }
+        }
+        else if (tokstate == TokState::S2)
+        {
+            if (std::isalpha(code[idx]) || std::isdigit(code[idx]))
+                tokstate = TokState::S2;
+            else if (std::isblank(code[idx]) || issymbol(code[idx]) || std::iscntrl(code[idx]))
+            {
+                Token tok(value);
+                /* check value */
+                if (iskeyword(value))
+                    tok.Type = TokType::tok_keyword;
+                else
+                    tok.Type = TokType::tok_identity;
+                res.push_back(tok);
+                value = "";
+                tokstate = TokState::S1;
+                idx--;
+                continue;
+            }
+            else
+            {
+                lexerror(idx, code[idx], tokstate);
+                continue;
+            }
+            
+        }
+        else if (tokstate == TokState::S3)
+        {
+            if (std::isdigit(code[idx]))
+                tokstate = TokState::S3;
+            else if (isdot(code[idx]))
+                tokstate = TokState::S4;
+            else if (std::isblank(code[idx]) || issymbol(code[idx]) || std::iscntrl(code[idx]))
+            {
+                Token tok(value, TokType::tok_int_num);
+                res.push_back(tok);
+                value = "";
+                tokstate = TokState::S1;
+                idx--;
+                continue;
+            }
+            else
+            {
+                lexerror(idx, code[idx], tokstate);
+                continue;
+            }
+        }
+        else if (tokstate == TokState::S4)
+        {
+            if (std::isdigit(code[idx]))
+                tokstate = TokState::S4;
+            else if (std::isblank(code[idx]) || issymbol(code[idx]) || std::iscntrl(code[idx]))
+            {
+                Token tok(value, TokType::tok_float_num);
+                res.push_back(tok);
+                value = "";
+                tokstate = TokState::S1;
+                idx--;
+                continue;
+            }
+            else
+            {
+                lexerror(idx, code[idx], tokstate);
+                continue;
+            }
+        }
+        else if (tokstate == TokState::S5)
+        {
+            if (issymbol(code[idx]))
+                tokstate = TokState::S5;
+            else if (std::isblank(code[idx]) || std::isdigit(code[idx]) || std::isalpha(code[idx]) || std::iscntrl(code[idx]))
+            {
+                Token tok(value);
+                /* need to add comment token */
+                res.push_back(tok);
+                value = "";
+                tokstate = TokState::S1;
+                idx--;
+                continue;
+            }
+            else
+            {
+                lexerror(idx, code[idx], tokstate);
+                continue;
+            }
+        }
+        else
+        {
+            std::fprintf(stderr, "lexing error, unknow state\r\n");
+            return res;
+        }
+        value += code[idx];
+    }
+    return res;
+}
+void deplog::lexerror(const int& idx, const char& c, TokState& tokstate)
+{
+    std::fprintf(stderr, "lexing error, idx=%d, char=%c, state=%d\r\n", idx, c, tokstate);
+    tokstate = TokState::S1;
+}
+const bool& deplog::iskeyword(const std::string& str)
+{
+    return str.c_str() == "for" || str.c_str() == "int";
 }
 const bool& deplog::iscomment_line(const std::string& str)
 {
@@ -39,63 +168,30 @@ const bool& deplog::issymbol(const char& c)
     /* check ASCII range */
     return ((0x21 <= c && c <= 0x2F) ||
         (0x3A <= c && c <= 0x40) || (0x5B <= c && c <= 0x60) ||
-        (0x7B <= c && c <= 0x7E)) && !(isdot(c) || isnext_line(c));
+        (0x7B <= c && c <= 0x7E)) && !(isdot(c) || std::iscntrl(c));
 }
 const bool& deplog::isdot(const char& c)
 {
     return c == '.';
 }
-static const std::vector<std::string> deplog::DictKeyWord = {
-    "for"
-};
-static const std::vector<std::string> deplog::DictOperator = {
-    "<",
-    ">",
-    "+",
-    "-"
-};
-static const std::vector<std::string> deplog::DictUnaryOperator = {
-    "++",
-    "--"
-};
 enum deplog::TokType
 {
     /* keyword */
     tok_keyword         ,   // when scanning an identity, check if it is a keyword
-    /* operator */
-    tok_less_than       ,   // <
-    tok_greater_than    ,   // >
-    /* unary operator */
-    tok_postfix_add     ,   // ++
-    tok_postfix_sub     ,   // --
-    /* assignment */
-    tok_assign          ,   // =
-    /* single symbol */
-    tok_single_symbol   ,
+    /* symbol */
+    tok_symbol          ,
     /* dynamic token */
     tok_identity        ,   // [a-zA-Z][a-zA-Z0-9_]*
     tok_int_num         ,   // -+[0-9]+
     tok_float_num       ,   // -+[0-9]+\.[0-9]*
-    tok_bytes               // 0x[0-9a-fA-F]+
+    tok_hex_num         ,   // 0x[0-9a-fA-F]+
+    /* control */
+    tok_ctrl
 };
 enum deplog::TokState
 {
     START,
-    LESS_THAN_1,    // <
-    GREATER_THAN_1, // >
-    ADD_1,          // +
-    ADD_2,          // ++
-    SUB_1,          // -
-    SUB_2,          // --
-    ASSIGN_1,       // =
-    ASSIGN_2,       // ==
-    PARENTHESE_L_1, // (
-    PARENTHESE_R_1, // )
-    BRACKET_L_1,    // [
-    BRACKET_R_2,    // ]
-    BRACE_L_1,      // {
-    BRACE_R_1,      // }
-    SEMI_1,         // ;
+    S1, S2, S3, S4, S5
 };
 class deplog::Token
 {
@@ -103,6 +199,8 @@ public:
     std::string Value;
     deplog::TokType Type;
     Token(){}
+    Token(std::string Value)
+        : Value(Value) {}
     Token(std::string Value, deplog::TokType Type)
         : Value(Value), Type(Type) {}
 };
